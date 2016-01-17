4fbcf46bac205d1a6f51b5caa2a00e9ed58a3b4f
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-01-17 17:33:57 -0600

    Add NavigationController.

---
+++ DeepThoughtsSwift/NavigationController.swift
@@ -0,0 +1,9 @@
+//
+//  NavigationController.swift
+//  DeepThoughtsSwift
+//
+//  Created by Jeremiah Kistler on 1/17/16.
+//  Copyright © 2016 Jeremiah Kistler. All rights reserved.
+//
+
+import Foundation
