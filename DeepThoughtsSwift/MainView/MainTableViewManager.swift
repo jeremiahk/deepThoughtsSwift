c080449c6e7819216685818c0dcfafd5db9c9dc2
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-01-17 15:11:26 -0600

    Use fakeData in the MainTableViewManager.

--- DeepThoughtsSwift/MainView/MainTableViewManager.swift
+++ DeepThoughtsSwift/MainView/MainTableViewManager.swift
@@ -9,19 +9,29 @@
 import UIKit
 
 class MainTableViewManager: NSObject, UITableViewDataSource, UITableViewDelegate {
+    let fakeData = FakeData()
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCellWithIdentifier("mainCell") as! MainTableViewCell
+        cell.sectionLabel.text = fakeData.categories[indexPath.row]
+        cell.titleLabel.text = fakeData.titles[indexPath.row]
+        
+        return cell
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return fakeData.titles.count
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 50
-    }
-
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let font = UIFont.preferredFontForTextStyle(UIFontTextStyleTitle2)
+        let attributes = [NSFontAttributeName: font]
+        let attributedString = NSAttributedString.init(string: self.fakeData.titles[indexPath.row], attributes: attributes)
         
+        let height = attributedString.boundingRectWithSize(CGSize(width: 299, height: 1000), options: [NSStringDrawingOptions.UsesLineFragmentOrigin, NSStringDrawingOptions.UsesFontLeading, NSStringDrawingOptions.UsesDeviceMetrics], context: nil).height
+        
+            NSLog("Height is \(height)")
+        
+        return height + 64.0;
     }
 }
