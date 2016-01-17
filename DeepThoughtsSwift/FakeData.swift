fedac85bf744f6f46e7a671d0004ce0ffa75abda
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-01-17 14:45:14 -0600

    Add fakeData struct.

---
+++ DeepThoughtsSwift/FakeData.swift
@@ -0,0 +1,15 @@
+//
+//  FakeData.swift
+//  DeepThoughtsSwift
+//
+//  Created by Jeremiah Kistler on 1/17/16.
+//  Copyright © 2016 Jeremiah Kistler. All rights reserved.
+//
+
+import Foundation
+
+struct fakeData {
+    let titles = ["Is there another word for 'synonym'?", "Is there another word for 'synonym'?1 alkdjf alskdjf alskdjflas dflkasd jflkasdj flka sdflk asjdflkjasdf asdfklj alksdjfkjsadf kasdjf asdkl;fjlk asdlkfjjsdlkjf adkfj kajsdf l;k", "Is there another"]
+    
+    let categories = ["Random thoughts", "Random thoughts1", "Random thoughts2"]
+}
