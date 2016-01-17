50a049e0794daf4e3e9887f203215f1a9c534371
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-01-17 14:35:27 -0600

    Add circleButton to project.

---
+++ DeepThoughtsSwift/CircleButton.swift
@@ -0,0 +1,13 @@
+//
+//  CircleButton.swift
+//  DeepThoughtsSwift
+//
+//  Created by Jeremiah Kistler on 1/17/16.
+//  Copyright © 2016 Jeremiah Kistler. All rights reserved.
+//
+
+import Cocoa
+
+class CircleButton: UIButton {
+
+}
