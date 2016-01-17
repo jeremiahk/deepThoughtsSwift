906fd1e050fe9767f8a0919073e26de3d9b78c5d
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-01-17 15:11:45 -0600

    Add support for circleButton in MainViewController.

--- DeepThoughtsSwift/MainView/MainViewController.swift
+++ DeepThoughtsSwift/MainView/MainViewController.swift
@@ -12,5 +12,16 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var circleButton: CircleButton!
     @IBOutlet weak var tableView: UITableView!
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        circleButton.shrink()
+    }
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        circleButton.animate()
+    }
 }
