04adbfd41c8896d499b9911c2d4fa60c8a4aa86e
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-01-17 16:54:09 -0600

    Add SettingsTableViewCell class.

---
+++ DeepThoughtsSwift/SettingsView/SettingsTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewCell.swift
+//  DeepThoughtsSwift
+//
+//  Created by Jeremiah Kistler on 1/17/16.
+//  Copyright © 2016 Jeremiah Kistler. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewCell: UITableViewCell {
+    @IBOutlet weak var categoryLabel: NSLayoutConstraint!
+}
