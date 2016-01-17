89b5db7a8916e2bb13623e5ab2c93ade1507a70e
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-01-17 14:18:44 -0600

    Add MainTableViewCell to project.

---
+++ DeepThoughtsSwift/MainView/MainTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MainTableViewCell.swift
+//  DeepThoughtsSwift
+//
+//  Created by Jeremiah Kistler on 1/17/16.
+//  Copyright © 2016 Jeremiah Kistler. All rights reserved.
+//
+
+import Cocoa
+
+class MainTableViewCell: UITableViewCell {
+
+}
