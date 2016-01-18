6ceb70148bd48ac3930c15cef0da0cf36ef36183
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-01-17 23:03:14 -0600

    Add thought struct to fakeData struct.

--- DeepThoughtsSwift/FakeData.swift
+++ DeepThoughtsSwift/FakeData.swift
@@ -8,8 +8,13 @@
 
 import Foundation
 
+public struct thought {
+    let title: String
+    let category: String
+}
+
 public struct FakeData {
-    public let titles = ["Is there another word for 'synonym'?", "I need this one to be kinda long so I can see if the font size calculation is right. I dont think it is. For some reason it is over estimating the size of the text", "Here is another"]
-    
-    public let categories = ["Random thoughts", "Random thoughts1", "Random thoughts2"]
+    let thoughts: [thought] = [thought(title: "Is there another word for 'synonym'?", category: "Random Thoughts"),
+                               thought(title: "I need this one to be kinda long so I can see if the font size calculation is right. It looks correct so I will just type some stuff here to make it way longer then it needs to be.", category: "Long Thought"),
+                               thought(title: "Here is another", category: "Another Category")]
 }
