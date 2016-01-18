390e24d0c57a87a70c7cc0da716fdc04267cfadc
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-01-17 23:03:56 -0600

    MainTableViewCell now uses CellConfigurator logic.

--- DeepThoughtsSwift/MainView/MainTableViewCell.swift
+++ DeepThoughtsSwift/MainView/MainTableViewCell.swift
@@ -8,9 +8,22 @@
 
 import UIKit
 
+extension MainTableViewCell: Updatable {
+    typealias ViewData = MainTableViewCellData
+}
+
+struct MainTableViewCellData {
+    let title: String
+    let category: String
+}
+
 class MainTableViewCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var sectionLabel: UILabel!
     
+    func updateWithViewData(viewData: ViewData) {
+        titleLabel.text = viewData.title
+        sectionLabel.text = viewData.category
+    }
 }
--- DeepThoughtsSwift/MainView/MainTableViewManager.swift
+++ DeepThoughtsSwift/MainView/MainTableViewManager.swift
@@ -9,26 +9,38 @@
 import UIKit
 
 class MainTableViewManager: NSObject, UITableViewDataSource, UITableViewDelegate {
-    let fakeData = FakeData()
+    private let cells: [CellConfiguratorType] = {
+        var cells: [CellConfiguratorType] = []
+        
+        for thought in FakeData().thoughts {
+            cells.append(CellConfigurator<MainTableViewCell>(viewData: MainTableViewCellData(title: thought.title, category: thought.category)))
+        }
+        
+        return cells
+    }()
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("mainCell") as! MainTableViewCell
-        cell.sectionLabel.text = fakeData.categories[indexPath.row]
-        cell.titleLabel.text = fakeData.titles[indexPath.row]
-        
+        let cellConfigurator = cells[indexPath.row]
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellConfigurator.reuseIdentifier, forIndexPath: indexPath)
+        cellConfigurator.updateCell(cell)
         return cell
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fakeData.titles.count
+        return cells.count
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        //TODO: This should not reinitialize the FakeData object. 
+        let text = FakeData().thoughts[indexPath.row].title
+        
         let font = UIFont.preferredFontForTextStyle(UIFontTextStyleTitle2)
         let attributes = [NSFontAttributeName: font]
-        let attributedString = NSAttributedString.init(string: self.fakeData.titles[indexPath.row], attributes: attributes)
+        let attributedString = NSAttributedString.init(string: text, attributes: attributes)
         
-        let height = attributedString.boundingRectWithSize(CGSize(width: 299, height: 1000), options: [NSStringDrawingOptions.UsesLineFragmentOrigin, NSStringDrawingOptions.UsesFontLeading, NSStringDrawingOptions.UsesDeviceMetrics], context: nil).height
+        let width = tableView.frame.size.width - 80
+        
+        let height = attributedString.boundingRectWithSize(CGSize(width: width, height: 1000), options: [NSStringDrawingOptions.UsesLineFragmentOrigin, NSStringDrawingOptions.UsesFontLeading, NSStringDrawingOptions.UsesDeviceMetrics], context: nil).height
         
         return height + 64.0;
     }
