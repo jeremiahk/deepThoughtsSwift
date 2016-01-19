3eacb6218ad23264703748e110cf54d2bffd8862
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-01-18 20:09:43 -0600

    Fix spelling.

--- DeepThoughtsSwift/UtilityFiles/UIColor.swift
+++ DeepThoughtsSwift/UtilityFiles/UIColor.swift
@@ -6,11 +6,11 @@
 import UIKit
 
 extension UIColor {
-    func rgb(r: CGFloat, b: CGFloat, g: CGFloat) -> UIColor {
+    func rbg(r: CGFloat, b: CGFloat, g: CGFloat) -> UIColor {
         return UIColor(red: r/255, green: b/255, blue: b/255, alpha: 1.0)
     }
 
-    func rgba(r: CGFloat, b: CGFloat, g: CGFloat, a: CGFloat) -> UIColor {
+    func rbga(r: CGFloat, b: CGFloat, g: CGFloat, a: CGFloat) -> UIColor {
         return UIColor(red: r/255, green: b/255, blue: b/255, alpha: a)
     }
 }
