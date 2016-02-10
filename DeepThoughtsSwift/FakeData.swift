f2ec9f1da31fc5851e33016f306269a41741b46b
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-02-09 22:31:32 -0600

    Add a bunch more fake data.

--- DeepThoughtsSwift/FakeData.swift
+++ DeepThoughtsSwift/FakeData.swift
@@ -13,5 +13,14 @@ public struct thought {
 public struct FakeData {
     let thoughts: [thought] = [thought(title: "Is there another word for 'synonym'?", category: "Random Thoughts"),
                                thought(title: "I need this one to be kinda long so I can see if the font size calculation is right. It looks correct so I will just type some stuff here to make it way longer then it needs to be.", category: "Long Thought"),
-                               thought(title: "Here is another", category: "Another Category")]
+                               thought(title: "Here is another", category: "Another Category"),
+    thought(title: "Here is another", category: "Another Category"),
+    thought(title: "Here is another", category: "Another Category"),
+    thought(title: "Here is another", category: "Another Category"),
+    thought(title: "Here is another", category: "Another Category"),
+    thought(title: "Here is another", category: "Another Category"),
+    thought(title: "Here is another", category: "Another Category"),
+    thought(title: "Here is another", category: "Another Category"),
+    thought(title: "Here is another", category: "Another Category"),
+    thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),thought(title: "Here is another", category: "Another Category"),]
 }
