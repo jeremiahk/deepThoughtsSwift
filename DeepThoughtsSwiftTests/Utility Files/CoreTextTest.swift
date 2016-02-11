29b004e9e8eb6591e5ed458a8d202443fa95b9a1
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-02-11 10:00:00 -0600

    Add CoreText class.

---
+++ DeepThoughtsSwiftTests/Utility Files/CoreTextTest.swift
@@ -0,0 +1,9 @@
+//
+//  CoreTextTest.swift
+//  DeepThoughtsSwift
+//
+//  Created by Jeremiah Kistler on 2/10/16.
+//  Copyright © 2016 Jeremiah Kistler. All rights reserved.
+//
+
+import Foundation
