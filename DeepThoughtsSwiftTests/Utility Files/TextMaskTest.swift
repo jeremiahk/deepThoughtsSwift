090ed13f655aa95823a0a9d82e31082d3bd4168e
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-02-11 21:22:38 -0600

    Add basic XCTest class.

--- DeepThoughtsSwiftTests/Utility Files/TextMaskTest.swift
+++ DeepThoughtsSwiftTests/Utility Files/TextMaskTest.swift
@@ -6,8 +6,23 @@
 //  Copyright © 2016 Jeremiah Kistler. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import DeepThoughtsSwift
 
-func test() {
+class TextMaskTest: XCTestCase {
     
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
 }
