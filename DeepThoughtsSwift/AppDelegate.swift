35ac65dc38e0e4fa11c4ac63e47a540d4d3f7ff9
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-04-02 17:55:56 -0500

    Fix sending dataController to wrong view controller.

--- DeepThoughtsSwift/AppDelegate.swift
+++ DeepThoughtsSwift/AppDelegate.swift
@@ -7,17 +7,17 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
     let dataController = DataController()
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         application.statusBarHidden = true
 
-        guard let controller = window?.rootViewController as? MainViewController else {
+        if let navigationController = window?.rootViewController as? UINavigationController, vc = navigationController.topViewController as? MainViewController {
+            vc.dataController = self.dataController
+        } else {
             fatalError("Unexpected vc in window")
         }
-        controller.dataController = dataController
 
         return true
     }
