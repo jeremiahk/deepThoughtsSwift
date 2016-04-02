42303629e52a04a4de5a1370b5f53528655fe4b0
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-04-02 13:06:22 -0500

    Add init with NSManagedObjectContext

--- DeepThoughtsSwift/Controllers/DataController.swift
+++ DeepThoughtsSwift/Controllers/DataController.swift
@@ -7,5 +7,30 @@ import Foundation
 import CoreData
 
 class DataController {
+    let managedObjectContext: NSManagedObjectContext
     
+    init() {
+        guard let modelURL = NSBundle.mainBundle().URLForResource("DataModel", withExtension: "momd") else {
+            fatalError("Unable to find model file")
+        }
+        guard let model = NSManagedObjectModel(contentsOfURL: modelURL) else {
+            fatalError("Failed to initilize data model")
+        }
+        let psc = NSPersistentStoreCoordinator(managedObjectModel: model)
+        managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = psc
+        
+        Queue.Background.execute() {
+            let filemanager = NSFileManager.defaultManager()
+            let docURL = filemanager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).last
+            guard let storeURL = docURL?.URLByAppendingPathComponent("data.sqlite") else {
+                fatalError("Unable to resolve store url")
+            }
+            do {
+                try psc.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: storeURL, options: nil)
+            } catch {
+                fatalError("Failed \(error)")
+            }
+        }
+    }
 }
