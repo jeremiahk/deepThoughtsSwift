41220ca165b10a2df8a1a70124fb9a7d4062d5c0
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-04-06 20:35:22 -0500

    Add network controller to project.

---
+++ DeepThoughtsSwift/Data/NetworkController.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  DeepThoughtsSwift
+//
+//  Created by Jeremiah Kistler on 4/5/16.
+//  Copyright © 2016 Jeremiah Kistler. All rights reserved.
+//
+
+import Foundation
+
+protocol: NetworkController {
+    func 
+}
