54d15f99e49e46c4d77af8b1255dc7832dc9aaf0
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-04-11 16:46:35 -0500

    Add a testNetworkController.

---
+++ DeepThoughtsSwiftTests/Data/TestNetworkController.swift
@@ -0,0 +1,9 @@
+//
+//  TestNetworkController.swift
+//  DeepThoughtsSwift
+//
+//  Created by Jeremiah Kistler on 4/10/16.
+//  Copyright © 2016 Jeremiah Kistler. All rights reserved.
+//
+
+import Foundation
