ab9fdaf06cb63cd22a738ace325efe850fbe958a
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-04-11 16:47:04 -0500

    Add networkProtocol.

--- DeepThoughtsSwiftTests/Data/TestNetworkController.swift
+++ DeepThoughtsSwiftTests/Data/TestNetworkController.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+class TestNetworkController: NSObject, NetworkProtocol {
+    private lazy var testData: [[String: AnyObject]] = self.getTestData()!
+    
+    private func getTestData() -> [[String: AnyObject]]? {
+        guard let dataPath = NSBundle.mainBundle().pathForResource("fakeData", ofType: "json") else {
+            fatalError("Could not find fakeData file")
+        }
+        guard let rawData = NSData(contentsOfFile: dataPath) else {
+            fatalError("Could not create NSData from fakeData file.")
+        }
+        
+        do {
+            guard let json = try NSJSONSerialization.JSONObjectWithData(rawData, options: .AllowFragments) as? [[String: AnyObject]] else {
+                fatalError("Could not cast fake json")
+            }
+            
+            return json
+        } catch {
+            print("Error creating test json: \(error)")
+        }
+        
+        return nil
+    }
+
+    func getAllThoughts(closure: [String : AnyObject] -> ()) {
+        
+    }
+    
+    func getAllCategories(closure: [String: AnyObject] -> ()) {
+        let titles = testData.flatMap(){ $0["title"] }
+        let finalData = ["categories": titles.description]
+        closure(finalData)
+    }
+}
